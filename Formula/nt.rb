aa30528c9e4851abb533b525aea24534e72603b1
Q Z <spellingbeeq@gmail.com>
2026-02-25 13:56:13 -0600

    nt v0.1.3 - add monthly activity bar chart

--- Formula/nt.rb
+++ Formula/nt.rb
@@ -1,8 +1,8 @@
 class Nt < Formula
   desc "Simple indexed notes maintainer for the command line"
   homepage "https://github.com/poiurewq/scripts"
-  url "https://github.com/poiurewq/scripts/archive/refs/tags/nt-v0.1.2.tar.gz"
-  sha256 "9cd5d7b715f69d5c902dee3f012430b2c3a1d256fdc7a1708241c2c535741557"
+  url "https://github.com/poiurewq/scripts/archive/refs/tags/nt-v0.1.3.tar.gz"
+  sha256 "048662da30a695c2a5d4720420860d2b46b39c935c78d37ccfd45926b8523090"
   license "MIT"
 
   depends_on "zsh" => :optional
