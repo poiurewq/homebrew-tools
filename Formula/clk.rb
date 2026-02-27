29e99156c71e37bfa88c1bfdc973028798473cc2
Q Z <spellingbeeq@gmail.com>
2026-02-27 01:33:05 -0600

    clk v0.2.5 - fix alias detection

--- Formula/clk.rb
+++ Formula/clk.rb
@@ -1,8 +1,8 @@
 class Clk < Formula
   desc "Clock in & out of work, tracking minutes spent per day"
   homepage "https://github.com/poiurewq/scripts"
-  url "https://github.com/poiurewq/scripts/archive/refs/tags/clk-v0.2.4.tar.gz"
-  sha256 "f2d10256fa31dac942fb1ad7694215ce520f7f6e782c60665d3d522c6cac14df"
+  url "https://github.com/poiurewq/scripts/archive/refs/tags/clk-v0.2.5.tar.gz"
+  sha256 "3289644346e4dcf36bc5eec871f3adf483f709d7f28d47f9d4e8489d1f22faef"
   license "MIT"
 
   depends_on "bash"
